9d2b331038c12005dc4ce184c5323a66147aae01
Cali93 <c.armut93@outlook.com>
2018-09-17 18:45:16 +0200

    server boilerplate done

--- server/app.js
+++ server/app.js
@@ -12,10 +12,6 @@ const mongoose = require("mongoose");
 
 const methodOverride = require('method-override');
 
-const userRoutes = require("./api/routes/user");
-const projectRoutes = require("./api/routes/projects");
-const productRoutes = require("./api/routes/products");
-const orderRoutes = require("./api/routes/orders");
 const uploadRoutes = require("./api/routes/upload");
 
 // Middleware
@@ -32,39 +28,14 @@ app.use(methodOverride('_method'));
 // MongoDB connection
 mongoose.Promise = global.Promise;
 
-const mongoURI = 'mongodb://localhost:27017/modsv2';
+const mongoURI = 'mongodb://localhost:27017/etfw';
 
 const debugDB = mongoose.connect(`${mongoURI}`)
         .then(() => console.log(`Connected on ${mongoURI}`))
         .catch((err) => console.log(`Database error: ${err}`));
 
 // Initialize Cors
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-
-// Use passport to handle authentication
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./api/config/passport')(passport);
-
-// Routes which should handle requests
-app.use("/user", userRoutes);
-app.use("/projects", projectRoutes)
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes);
-app.use("/upload", uploadRoutes);
+app.use(cors());
 
 // Index Route
 app.get('/', (req, res) => {
